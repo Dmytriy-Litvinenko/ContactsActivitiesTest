4c6167b3e35035d743764b6b5c52a23bd3bd6dbf
Dmitriy Lytvynenko <d.lytvynenko@aimprosoft.com>
2017-12-22 20:23:29 +0200

    squash

--- src/main/java/dima/litvinenko/controllers/SquashController.java
+++ src/main/java/dima/litvinenko/controllers/SquashController.java
@@ -5,5 +5,8 @@ private String property1;
 private String property2;
 private String property3;
 private String property4;
+    private String property5;
+    private String property6;
+    private String property7;
 
 }
