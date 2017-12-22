91287fb2cdab74459e60c5e0e4dbd671d4b186d9
Dmitriy Lytvynenko <d.lytvynenko@aimprosoft.com>
2017-12-22 07:49:19 +0200

    branch1 property4

--- src/main/java/dima/litvinenko/controllers/SquashController.java
+++ src/main/java/dima/litvinenko/controllers/SquashController.java
@@ -3,5 +3,7 @@ package dima.litvinenko.controllers;
 public class SquashController {
 private String property1;
 private String property2;
-    private String property3;
+private String property3;
+private String property4;
+
 }
